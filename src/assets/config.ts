055fe16b36e9830f8eb2efce58a0ad007bbcbc88
Bogdan21o312 <lutskiv2010@gmail.com>
2023-03-16 20:48:19 +0200

    test

---
+++ src/assets/config.ts
@@ -0,0 +1,27 @@
+import Arrow from "./icons/arrow.svg"
+import Cart from "./icons/cart.svg"
+import Location from "./icons/location.svg"
+import Love from "./icons/love.svg"
+import Search from "./icons/search.svg"
+import Suitcase from "./icons/suitcase.svg"
+import Star from "./icons/star.svg"
+import Plus from "./icons/plus.svg"
+import Cargo from "./icons/cargo.svg"
+import Choose from  "./icons/choose.svg"
+import SuccessMark from "./icons/success-mark.svg"
+import DiscountMark from "./icons/discount-mark.svg"
+
+export {
+    Arrow,
+    Cart,
+    Location,
+    Love,
+    Search,
+    Suitcase,
+    Star,
+    Plus,
+    Choose,
+    Cargo,
+    SuccessMark,
+    DiscountMark
+}
